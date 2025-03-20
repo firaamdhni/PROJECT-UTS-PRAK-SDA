c2bfa4ef446f4fdad176baf59a6c8db3422dc39e
annisahaura <annisa355@mhs.usk.ac.id>
2025-03-20 12:56:05 +0700

    Update parsing_expression.c
    Signed-off-by: annisahaura <annisa355@mhs.usk.ac.id>

--- parsing_expression.c
+++ parsing_expression.c
@@ -37,3 +37,61 @@ int isValidInfix(char *exp) {
     }
     return (operand > operator); // Operand harus lebih banyak atau sama
 }
+
+// Fungsi konversi ekspresi
+void reverseString(char *str) {
+    int n = strlen(str);
+    for (int i = 0; i < n / 2; i++) {
+        char temp = str[i];
+        str[i] = str[n - i - 1];
+        str[n - i - 1] = temp;
+    }
+}
+
+void infixToPostfix(char *exp, char *result) {
+    Stack s;
+    initialize(&s);
+    int k = 0;
+    for (int i = 0; exp[i]; i++) {
+        if (isalnum(exp[i])) result[k++] = exp[i];
+        else if (exp[i] == '(') push(&s, "(");
+        else if (exp[i] == ')') {
+            while (!isEmpty(&s) && strcmp(peek(&s), "(") != 0) result[k++] = pop(&s)[0];
+            pop(&s);
+        } else {
+            while (!isEmpty(&s) && precedence(peek(&s)[0]) >= precedence(exp[i])) result[k++] = pop(&s)[0];
+            char temp[2] = {exp[i], '\0'};
+            push(&s, temp);
+        }
+    }
+    while (!isEmpty(&s)) result[k++] = pop(&s)[0];
+    result[k] = '\0';
+}
+
+void postfixToInfix(char *exp, char *result) {
+    Stack s;
+    initialize(&s);
+    for (int i = 0; exp[i]; i++) {
+        if (isalnum(exp[i])) {
+            char operand[2] = {exp[i], '\0'};
+            push(&s, operand);
+        } else {
+            char op2[MAX], op1[MAX], temp[MAX];
+            strcpy(op2, pop(&s));
+            strcpy(op1, pop(&s));
+            sprintf(temp, "(%s%c%s)", op1, exp[i], op2);
+            push(&s, temp);
+        }
+    }
+    strcpy(result, pop(&s));
+}
+
+void infixToPrefix(char *exp, char *result) {
+    reverseString(exp);
+    for (int i = 0; exp[i]; i++) {
+        if (exp[i] == '(') exp[i] = ')';
+        else if (exp[i] == ')') exp[i] = '(';
+    }
+    infixToPostfix(exp, result);
+    reverseString(result);
+}
